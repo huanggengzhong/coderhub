aea53835710aad8694bd155e25b2b8ac0291407e
huanggengzhong <915835528@qq.com>
2021-03-16 23:05:12 +0800

    推送最新代码

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-const app = require("./app")
-require("./app/database")
-const config = require("./app/config")
+const app = require("./app");
+require("./app/database");
+const config = require("./app/config");
 app.listen(config.APP_PORT, () => {
+  console.log("2021年3月16日23:04:44测试");
   console.log(`服务器在${config.APP_PORT}端口启动成功~`);
-})
+});
