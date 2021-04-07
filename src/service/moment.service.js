1ce29caae0b987182caead482ffcbb5e2ebea378
huanggengzhong <915835528@qq.com>
2021-04-07 23:15:35 +0800

    feat:finish biaoqian+pinglun

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -9,25 +9,48 @@ class MomentService {
   }
   // 某一条动态明细
   async detail(id) {
-    const statement = `SELECT m.id id,m.content content ,m.createAt createTime,m.updateAt updateTime,
-      JSON_OBJECT('id',u.id,'name',u.name) author,
-      (SELECT COUNT(*) FROM comment c WHERE c.moment_id=m.id) commentCount
-      FROM moment m
-      LEFT JOIN user u ON m.user_id=u.id
-      WHERE m.id=?;`;
+    const statement = `
+    SELECT 
+    m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
+    JSON_OBJECT('id', u.id, 'name', u.name) author,
+    IF(COUNT(l.id),JSON_ARRAYAGG(
+      JSON_OBJECT('id', l.id, 'name', l.name)
+    ),NULL) labels,
+    (SELECT IF(COUNT(c.id),JSON_ARRAYAGG(
+      JSON_OBJECT('id', c.id, 'content', c.content, 'commentId', c.comment_id, 'createTime', c.creatAt,
+                  'user', JSON_OBJECT('id', cu.id, 'name', cu.name))
+    ),NULL) FROM comment c LEFT JOIN user cu ON c.user_id = cu.id WHERE m.id = c.moment_id) comments
+  FROM moment m
+  LEFT JOIN user u ON m.user_id = u.id
+  LEFT JOIN moment_label ml ON m.id = ml.moment_id
+  LEFT JOIN label l ON ml.label_id = l.id
+  WHERE m.id = ?
+  GROUP BY m.id;  
+    `;
+    // 使用了JSON_ARRAYAGG,一定要分组
+    // const statement = `SELECT m.id id,m.content content ,m.createAt createTime,m.updateAt updateTime,
+    //   JSON_OBJECT('id',u.id,'name',u.name) author,
+
+    //   FROM moment m
+    //   LEFT JOIN user u ON m.user_id=u.id
+    //   WHERE m.id=?;`;
     // const statement = "SELECT * FROM moment m WHERE m.id=?;";
     const result = await connect.execute(statement, [id]);
+    console.log(result, "result");
     return result[0];
   }
   // 动态列表
   async getList(data) {
     console.log(data.offset, data.size, "data");
     try {
-      const statement = `SELECT m.id id,m.content content ,m.createAt createTime,m.updateAt updateTime,
-      JSON_OBJECT('id',u.id,'name',u.name) author,
-      (SELECT COUNT(*) FROM comment c WHERE c.moment_id=m.id) commentCount
-      FROM moment m
-      LEFT JOIN user u ON m.user_id=u.id
+      const statement = `
+      SELECT 
+      m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
+      JSON_OBJECT('id', u.id, 'name', u.name) author,
+      (SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount,
+      (SELECT COUNT(*) FROM moment_label ml WHERE ml.moment_id = m.id) labelCount
+    FROM moment m
+    LEFT JOIN user u ON m.user_id = u.id
       LIMIT ${data.offset},${data.size};`;
       const result = await connect.execute(statement, []); //分页查询要写在sql语句中,不然有bug
       // const result = await connect.execute(statement, [data.size, data.offset]);
